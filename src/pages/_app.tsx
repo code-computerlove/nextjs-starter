116e11460f549117d0af6f7871ea3ea00e180888
Lucile Vacquie <lucile.vacquie@codecomputerlove.com>
2022-11-03 13:38:38 +0000

    feat: moved postcss dep into dev dep and added postcss instructions into runbook

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 import dynamic from 'next/dynamic';
 
 const MockApi = dynamic(() => import('./mock-api'));
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return (
-    <>
-      <MockApi />
-      <Component {...pageProps} />
-    </>
-  );
+	return (
+		<>
+			<MockApi />
+			<Component {...pageProps} />
+		</>
+	);
 };
 
 export default MyApp;
-
