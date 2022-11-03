9fc6cc49e3e36573308382cc7df172ab2f70610e
Alex Clapperton <hi@alexclapperton.co.uk>
2022-11-03 11:52:18 +0000

    chore: 🔧 Update next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const path = require('path');
-
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'src/styles')],
-    prependData: `@import "variables";`,
-  },
-}
+	reactStrictMode: true,
+	swcMinify: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
